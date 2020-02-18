40962b6bfd43306a1b36b8323e30273859505b91
Student <example@example.com>
2020-02-18 14:55:27 -0800

    update/style Topic component, add event handler for upvoting

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Topic(props){
-  return(
-    <div>
-    <h1>{props.title}</h1>
-    <p>{props.author}</p>
-    <p>{props.content}</p>
-    </div>
-  )
+class Topic extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.topicListOnVote();
+  };
+
+  render(){
+    const issueDiv = {
+      border: '2px solid white',
+      padding: '8px',
+      borderRadius: '4px',
+      textAlign: 'center',
+      width: '80%',
+      margin: '0 auto',
+      marginTop: "16px"
+    }
+
+    const titleHR = {
+      // width: '80%'
+    }
+
+    const italics = {
+      fontStyle: 'italic'
+    }
+    return(
+      <div style={issueDiv}>
+        <h1>Issue: {this.props.title}</h1>
+        <hr style={titleHR}/>
+        <p>{this.props.content}</p>
+        <p>submitted by: <span style={italics}>{this.props.author}</span></p>
+        <p onClick={this.handleClick}>Upvote </p>
+      </div>
+    )
+  }
 }
 
 Topic.propTypes = {title: PropTypes.string, author: PropTypes.string, content: PropTypes.string}
+
+export default Topic;
+// <p onClick={this.downvote(props.id)}>Upvote {this.state.downvotes}</p>
