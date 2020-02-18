31020a9a4f6c276b717637bb8009305acce5aca1
Student <example@example.com>
2020-02-18 14:53:38 -0800

    pass props/event handler to TopicList/Topic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import './App.css'
 
 // Components
 import Header from './components/Header';
 import TopicList from './components/TopicList';
-import NewTopicForm from './components/NewTopicForm';
 import NewTopicFormControl from './components/NewTopicFormControl';
+import ConflictResolution from './components/ConflictResolution';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,7 @@ export default class App extends React.Component {
       masterTopicList: []
     }
     this.handleAddingNewTopic = this.handleAddingNewTopic.bind(this);
+    this.handleVote = this.handleVote.bind(this);
   }
 
   handleAddingNewTopic(newTopic) {
@@ -21,14 +23,23 @@ export default class App extends React.Component {
       masterTopicList: [...this.state.masterTopicList, newTopic]
     });
   }
+
+  handleVote(id, modifier){
+    console.log('fuck jim');
+  }
+
+
   render() {
   return (
     <div>
     <Header/>
     <Switch>
-    <Route exact path='/' render={()=>(<TopicList topicList={this.state.masterTopicList} /> )} />
+    <Route exact path='/' render={()=>(<TopicList
+        topicList={this.state.masterTopicList}
+        onVote={this.handleVote}/> )} />
+    <Route path='/conflictresolution' component={ConflictResolution}></Route>
     <Route exact path='/newtopic' render={() => (
-      <NewTopicForm
+      <NewTopicFormControl
         onNewTopicCreation={this.handleAddingNewTopic} />
     )}/>
     </Switch>
