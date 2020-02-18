70c5a2e7ec0d06499955fe3a50bee9dd190bbc16
Student <example@example.com>
2020-02-18 14:55:52 -0800

    add complaint array, pass upvote props to Topic

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -3,11 +3,23 @@ import Topic from './Topic';
 import PropTypes from 'prop-types';
 
 export default function TopicList(props) {
+  const complaints = [
+    {
+      title: "Why Jim Sucks",
+      author: "Dwight Schrute",
+      content: "He keeps putting my things in Jello",
+      id: 0
+    }
+  ]
+
+
   return(
     <div>
     {props.topicList.map((topic, index) => {
-      return <Topic {...topic} key={index} />
+      return <Topic {...topic} key={index} topicListOnVote={props.onVote}/>
     })}
+    {complaints.map((topic, index) => <Topic {...topic} key={index} topicListOnVote={props.onVote}/>
+    )}
     </div>
   )
 }
