a02fb557029137e5bc30e7538b9204ed1bf04e7f
Student <example@example.com>
2020-02-18 14:54:27 -0800

    add KevinConfirmation page when submitting Topic

--- src/components/KevinConfirmation.jsx
+++ src/components/KevinConfirmation.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function KevinConfirmation() {
+export default function KevinConfirmation(props) {
+  return(
+    <div>
+      <p>Have you consulted <Link to='/conflictresolution'>Toby's Conflict Resolution</Link> binder?</p>
+      <button onClick={props.onKevinConfirmation}>Yes</button>
+    </div>
+  );
+}
 
+KevinConfirmation.propTypes = {
+  onKevinConfirmation: PropTypes.func
 }
